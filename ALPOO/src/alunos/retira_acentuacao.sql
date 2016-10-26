7028e47a4a443fff70f6d73c29ff9fa7c7a40376
IgorThierry <igorthierry15@gmail.com>
2016-10-26 17:10:45 -0200

    função ER para remoção de acentos

---
+++ ALPOO/src/alunos/retira_acentuacao.sql
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  HJSYSTEMS
+ * Created: 26/10/2016
+ */
+
+CREATE OR REPLACE FUNCTION retira_acentuacao(p_texto text)  
+  RETURNS text AS  
+ $BODY$  
+ Select translate($1,  
+ 'áàâãäåaaaÁÂÃÄÅAAAÀéèêëeeeeeEEEÉEEÈìíîïìiiiÌÍÎÏÌIIIóôõöoooòÒÓÔÕÖOOOùúûüuuuuÙÚÛÜUUUUçÇñÑýÝ',  
+ 'aaaaaaaaaAAAAAAAAAeeeeeeeeeEEEEEEEiiiiiiiiIIIIIIIIooooooooOOOOOOOOuuuuuuuuUUUUUUUUcCnNyY'   
+  );  
+ $BODY$  
+ LANGUAGE sql VOLATILE  
+ COST 100;  
